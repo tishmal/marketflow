8388727e109ad55a6fade6941e45ccd3a3284650
tishmal <arturtimur201998@gmail.com>
2025-05-14 21:21:32 +0500

    feat: init base main.go

--- cmd/marketflow/main.go
+++ cmd/marketflow/main.go
@@ -1,5 +1,21 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+)
+
 func main() {
-	
+	port := flag.Int("port", 8080, "port number")
+	help := flag.Bool("help", false, "show usage")
+	flag.Parse()
+
+	if *help {
+		flag.Usage()
+
+		return
+	}
+	// initial interfaces
+
+	fmt.Printf("Starting on:%d ...\n", *port)
 }
