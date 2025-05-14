e87a5de4b13ee9e599add79eac28889babe50279
tishmal <arturtimur201998@gmail.com>
2025-05-14 23:53:14 +0500

    refactor: apply composition to price models

--- internal/domain/model/models.go
+++ internal/domain/model/models.go
@@ -2,18 +2,22 @@ package domain
 
 import "time"
 
+// метаданые о рынке
+type MetaMarket struct {
+	Exchange string // Имя биржи
+	Pair     string // Торговая пара (например, "BTC/USD")
+}
+
 // обновление цен
 type PriceUpdate struct {
-	Exchange string    // Имя биржи
-	Pair     string    // Торговая пара (например, "BTC/USD")
-	Price    float64   // Текущая цена
-	Time     time.Time // Время обновления
+	MetaMarket
+	Price float64   // Текущая цена
+	Time  time.Time // Время обновления
 }
 
 // статистика цен
 type PriceStats struct {
-	Exchange  string
-	Pair      string
+	MetaMarket
 	Timestamp time.Time // Время создания статистики
 	Average   float64   // Средняя цена
 	Min       float64   // Минимальная цена
